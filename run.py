3f7289588859d57cf0459d523c6f8c27e6b8cd93
aviapollyon <aviapollyon@gmail.com>
2026-03-11 15:56:40 +0200

    updated readme

--- run.py
+++ run.py
@@ -6,12 +6,7 @@ try:
     import flask_mail
 except ModuleNotFoundError:
     print(
-        "\nERROR: flask_mail is not installed.\n"
-        "Please run the server using the project's virtual environment:\n"
-        r"  .\venv\Scripts\python.exe run.py"
-        "\nor activate it first:\n"
-        r"  .\venv\Scripts\Activate.ps1"
-        "\n  python run.py\n",
+        "\nERROR: flask_mail is not installed.\n",
         file=sys.stderr,
     )
     sys.exit(1)
